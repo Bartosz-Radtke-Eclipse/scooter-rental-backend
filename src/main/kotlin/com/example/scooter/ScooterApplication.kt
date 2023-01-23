504cced2e5a7f3486d225aaba2d1d0c658409ca4
Bartosz-Radtke-Eclipse <bartosz.radtke@eclipsegroup.co.uk>
2023-01-23 22:43:06 +0100

    fix cors

--- src/main/kotlin/com/example/scooter/ScooterApplication.kt
+++ src/main/kotlin/com/example/scooter/ScooterApplication.kt
@@ -2,10 +2,29 @@ package com.example.scooter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
 
 @SpringBootApplication
 class ScooterApplication
 
 fun main(args: Array<String>) {
     runApplication<ScooterApplication>(*args)
+}
+
+@Bean
+fun corsFilter(): CorsFilter {
+    val urlBasedCorsConfigurationSource = UrlBasedCorsConfigurationSource()
+    val corsConfiguration = CorsConfiguration()
+    corsConfiguration.allowCredentials = true
+    corsConfiguration.allowedOrigins = listOf(
+        "http://localhost:3000"
+    )
+
+    corsConfiguration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+    urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration)
+    return CorsFilter(urlBasedCorsConfigurationSource)
 }
